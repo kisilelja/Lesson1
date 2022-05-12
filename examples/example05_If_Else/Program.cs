c001c6c4d6db80401961f6ae8114c6612d4715d2
Olga <o.o.kiseleva@yandex.ru>
2022-05-12 14:05:35 +0300

    add examples/example05_If_Else/

---
+++ examples/example05_If_Else/Program.cs
@@ -0,0 +1,12 @@
+﻿Console.Write("ВВедите Ваше Имя: ");
+string username = Console.ReadLine();
+
+if (username.ToLower () == "маша")
+{
+    Console.WriteLine("Ура, Это же Маша!");
+}
+else 
+{
+    Console.Write("Привет, ");
+    Console.WriteLine(username);
+}
